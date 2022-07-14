fc23b69319ac5e91fcc902051f98f864e26c375b
Frank Gerngross <f.gerngross@googlemail.com>
2022-07-14 20:59:13 +0200

    created all missing methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,24 @@
 class ReviewsController < ApplicationController
-  resources :restaurants do
-    resources _reviews, only: [ :new, :create ]
+  before_action :find_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
